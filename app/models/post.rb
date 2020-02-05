6e170bce819719aa39b3536847da35ecdf88c5f8
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-05 22:09:30 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
-  validates :category, length: { is: 1 }
+  validates :category
 end
